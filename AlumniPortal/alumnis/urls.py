5ceae77db3236865c9d93aa66fa28fbbb1d1f569
ajil2545 <ajilkunjumon2000@gmail.com>
2022-10-12 20:47:21 +0530

    commit

--- AlumniPortal/alumnis/urls.py
+++ AlumniPortal/alumnis/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('alumniReg/', views.alumniReg, name='alumniReg'),
     path('alumniReg/addUser/', views.addUser, name='addUser'),
+    path('delete/<int:id>', views.delete, name='delete'),
+    path('update/<int:id>', views.update, name='update'),
+    path('update/updaterecord/<int:id>', views.updaterecord, name='updaterecord'),
 ]
--- AlumniPortal/alumnis/views.py
+++ AlumniPortal/alumnis/views.py
@@ -21,3 +21,25 @@ def addUser(request):
   a = Alumnis(email=x, password=y)
   a.save()
   return HttpResponseRedirect(reverse('index'))
+
+def delete(request, id):
+  a = Alumnis.objects.get(id=id)
+  a.delete()
+  return HttpResponseRedirect(reverse('index'))
+
+def update(request, id):
+  a = Alumnis.objects.get(id=id)
+  template = loader.get_template('update.html')
+  context = {
+    'myalumnis': a,
+  }
+  return HttpResponse(template.render(context, request))
+
+def updaterecord(request, id):
+  mail = request.POST['mail']
+  psw = request.POST['psw']
+  al = Alumnis.objects.get(id=id)
+  al.email = mail
+  al.password = psw
+  al.save()
+  return HttpResponseRedirect(reverse('index'))
